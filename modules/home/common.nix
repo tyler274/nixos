7be00164f7ed1740172556692de364114165cc97
tyler <tyler274port@gmail.com>
2026-05-04 13:41:37 +0000

    done

--- modules/home/common.nix
+++ modules/home/common.nix
@@ -78,15 +78,9 @@
     enableDefaultConfig = false;
     matchBlocks."*" = {
       addKeysToAgent = "yes";
-      forwardAgent = false;
-      compression = false;
-      serverAliveInterval = 0;
-      serverAliveCountMax = 3;
-      hashKnownHosts = false;
-      userKnownHostsFile = "~/.ssh/known_hosts";
-      controlMaster = "no";
+      controlMaster = "auto";
       controlPath = "~/.ssh/master-%r@%n:%p";
-      controlPersist = "no";
+      controlPersist = "10m";
     };
   };
 
