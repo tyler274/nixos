a10b262dc26e1327c7f05a2f5b07d834e0710d61
tyler <tyler274port@gmail.com>
2026-05-24 00:08:38 -0400

    fix typo

--- hosts/cyrene/zfs.nix
+++ hosts/cyrene/zfs.nix
@@ -3,7 +3,7 @@
 {
   boot.supportedFilesystems = [ "zfs" "ntfs" ];
   boot.zfs.forceImportRoot = false;
-  boot.zfs.requestEncryp------------------------------q                                                                                                                                                                                                                                                                                                                                                                                       qqqqqP[---------------KIOJJ9***************************************
+  boot.zfs.requestEncryptionCredentials = "prompt"                                                                                                                                                                                                                                                                                                                                                                                      qqqqqP[---------------KIOJJ9***************************************
 
   networking.hostId = "48cd5bc1";
 
