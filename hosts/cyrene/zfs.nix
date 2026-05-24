db830689e0af42bcfb5c31ece841d30baadf7ea6
tyler <tyler274port@gmail.com>
2026-05-24 00:33:03 -0400

    keep kernels recent

--- hosts/cyrene/zfs.nix
+++ hosts/cyrene/zfs.nix
@@ -1,12 +1,27 @@
 { config, pkgs, lib, ... }:
 
+let
+  zfsCompatibleKernelPackages = lib.filterAttrs (
+    name: kernelPackages:
+    (builtins.match "linux_[0-9]+_[0-9]+" name) != null
+    && (builtins.tryEval kernelPackages).success
+    && (!kernelPackages.${config.boot.zfs.package.kernelModuleAttribute}.meta.broken)
+  ) pkgs.linuxKernel.packages;
+  latestKernelPackage = lib.last (
+    lib.sort (a: b: (lib.versionOlder a.kernel.version b.kernel.version)) (
+      builtins.attrValues zfsCompatibleKernelPackages
+    )
+  );
+in
 {
   boot.supportedFilesystems = [ "zfs" "ntfs" ];
   boot.zfs.forceImportRoot = false;
   boot.zfs.requestEncryptionCredentials = [ "prompt" ];
   networking.hostId = "48cd5bc1";
 
-  boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+  
+  # Note this might jump back and forth as kernels are added or removed.
+  boot.kernelPackages = latestKernelPackage;
 
   boot.loader = {
     efi = {
