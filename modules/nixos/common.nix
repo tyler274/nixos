0273fbbb65da3949db91ca00533a73d5d5696f0d
tyler274 <tyler274port@gmail.com>
2026-06-04 23:10:22 -0700

    try re-enabling mimalloc

--- modules/nixos/common.nix
+++ modules/nixos/common.nix
@@ -45,7 +45,7 @@
 
   security.rtkit.enable = true;
 
-  environment.memoryAllocator.provider = "libc";
+  environment.memoryAllocator.provider = "mimalloc";
 
   # Enable mimalloc's hardened build: randomises heap segment placement,
   # adds guard pages, and validates free-list integrity. Trades a small
