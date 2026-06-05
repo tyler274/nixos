5a993b48a504fccf3e220fee087ef32ee35e20ad
tyler274 <tyler274port@gmail.com>
2026-06-04 23:20:42 -0700

    no mimalloc rn

--- modules/nixos/common.nix
+++ modules/nixos/common.nix
@@ -45,7 +45,7 @@
 
   security.rtkit.enable = true;
 
-  environment.memoryAllocator.provider = "mimalloc";
+  environment.memoryAllocator.provider = "libc";
 
   # Enable mimalloc's hardened build: randomises heap segment placement,
   # adds guard pages, and validates free-list integrity. Trades a small
